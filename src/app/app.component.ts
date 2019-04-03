a2fba3beb8164276706dfa3f9a500d9e479d7988
BrenanWright <bjwright2@crimson.ua.edu>
2019-04-02 23:25:39 -0500

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,14 @@ export class AppComponent {
   removeModal() {
     this.modalService.destroy();
   }
+
+  ngOnInit(){
+    fetch("https://imbrenan.azurewebsites.net/api/auth/check", {method:"GET", mode:"cors", headers:{"Authorization": `Bearer ${localStorage.getItem("auth_token")}`}})
+    .then(function(response){
+      if(response.status==401)
+      {
+        localStorage.removeItem("auth_token");
+      }
+    })
+  }
 }
