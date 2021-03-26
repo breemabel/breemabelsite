97a2a0f9d336d386a64bac57598f6a2d16eca912
Brenan Wright <codeofconductz@gmail.com>
2021-03-26 16:47:00 -0400

    Merge branch 'master' of https://github.com/BrenanWright/imbrenanangular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,18 @@ import { ModalService } from '../app/shared/services/modal.service'
 export class AppComponent {
   title = 'ImBrenanAngular';
   modalService :ModalService;
-  
+
   removeModal() {
     this.modalService.destroy();
   }
 
   ngOnInit(){
-    fetch("https://imbrenan.azurewebsites.net/api/auth/check", {method:"GET", mode:"cors", headers:{"Authorization": `Bearer ${localStorage.getItem("auth_token")}`}})
+/*     fetch("https://imbrenan.azurewebsites.net/api/auth/check", {method:"GET", mode:"cors", headers:{"Authorization": `Bearer ${localStorage.getItem("auth_token")}`}})
     .then(function(response){
       if(response.status==401)
       {
         localStorage.removeItem("auth_token");
       }
-    })
+    }) */
   }
 }
