9feeba773aac2c484da991b97201e38c0f7fe12f
Brenan Wright <codeofconductz@gmail.com>
2021-03-26 16:25:20 -0400

    Merge branch 'master' of https://github.com/BrenanWright/imbrenanangular

---
+++ src/app/shared/models/venue.interface.ts
@@ -0,0 +1,10 @@
+export interface Venue {
+  city: String;
+  address1: String;
+  address2: String;
+  displayAddress: String;
+  name: String;
+  id: number;
+  state: String;
+  country: String;
+}
